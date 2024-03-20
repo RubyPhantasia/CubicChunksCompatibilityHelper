fb4093ea6da84bdf3cffab320020f2631276cc2b
= <=>
2024-03-19 22:35:25 -0700

    Changed build.gradle so it uses ant's ReplaceTokens to replace tokens in source code with the corresponding value in gradle.properties * E.g. it replaces @MOD_VERSION@ with the modVersion in gradle.properties * No longer have to hunt down each instance of a version number

--- src/main/java/com/rubyphantasia/cubic_chunks_compatibility_helper/ModInfo.java
+++ src/main/java/com/rubyphantasia/cubic_chunks_compatibility_helper/ModInfo.java
@@ -15,11 +15,11 @@
 package com.rubyphantasia.cubic_chunks_compatibility_helper;
 
 public class ModInfo {
-    public static final String MODID = "cubic_chunks_compatibility_helper";
-    public static final String NAME = "Cubic Chunks Compatibility Helper";
+    public static final String MODID = "@MODID@";
+    public static final String NAME = "@MOD_NAME@";
     public static final String MOD_ABBREVIATION = "ccch";
     public static final String MOD_ABBREVIATION_PREFIX = MOD_ABBREVIATION+"_";
-    public static final String VERSION = "0.1.0-SNAPSHOT";
+    public static final String VERSION = "@MOD_VERSION@";
     public static final String packageName = "com.rubyphantasia.cubic_chunks_compatibility_helper";
 
     public static final String MODULE_CONFIGS_PATH_PREFIX = MODID+"_moduleConfigs/module_";
